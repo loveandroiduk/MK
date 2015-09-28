aa65d6f636f23005be96e0e3e469c751f5655fdc
metalkettle <metalkettle999@gmail.com>
2015-09-28 18:22:29 +0100

    Online Movies 1.2.0

--- plugin.video.onlinemovies/default.py
+++ plugin.video.onlinemovies/default.py
@@ -4,7 +4,6 @@ from metahandler import metahandlers
 
 addon_id = 'plugin.video.onlinemovies'
 selfAddon = xbmcaddon.Addon(id=addon_id)
-metaget = metahandlers.MetaData(preparezip=False)
 addon = Addon(addon_id, sys.argv)
 ADDON2=xbmcaddon.Addon(id='plugin.video.onlinemovies')
 fanart = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id , 'fanart.jpg'))
@@ -12,29 +11,30 @@ icon = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'ico
 metaset = selfAddon.getSetting('enable_meta')
 
 def CATEGORIES():
-        addDir2('Latest Cinema Releases','http://watch-onlinemovies.pro/?display=tube&filtre=date',1,icon,'',fanart)
-        addDir2('Most Viewed','http://watch-onlinemovies.pro/?display=tube&filtre=views',1,icon,'',fanart)
-        addDir2('Highest Rated','http://watch-onlinemovies.pro/?display=tube&filtre=rate',1,icon,'',fanart)
+        addDir2('Latest Cinema Releases','http://onlinemovies.pro/category/most-popular-new-movies/?filtre=date',1,icon,'',fanart)
+        addDir2('Recently Added','http://onlinemovies.pro/category/genre/?filtre=date',1,icon,'',fanart)
+        addDir2('Most Viewed','http://onlinemovies.pro/category/genre/?filtre=views',1,icon,'',fanart)
+        addDir2('Highest Rated','http://onlinemovies.pro/category/genre/?filtre=rate',1,icon,'',fanart)
+        addDir2('HD Movies','http://onlinemovies.pro/category/hd-movies/?filtre=random',1,icon,'',fanart) 
+        addDir2('Christmas Movies','http://onlinemovies.pro/category/christmas-movies/',1,icon,'',fanart)
+        addDir2('Disney','http://onlinemovies.pro/category/disneys/',1,icon,'',fanart)
+        addDir2('Latest TV Episodes','http://onlinemovies.pro/category/serials/?filtre=date',1,icon,'',fanart) 
         addDir2('Search','url',3,icon,'',fanart)
         xbmc.executebuiltin('Container.SetViewMode(50)')
                
 def GETMOVIES(url,name):
         metaset = selfAddon.getSetting('enable_meta')
         link = open_url(url)
-        match=re.compile('<a href="(.+?)" title="(.+?)">').findall(link)[:-13]
-        if 'serials' in url:
-                match=re.compile('<a href="(.+?)" title="(.+?)">').findall(link)[:-13]
-                metaset='false'
+        if 'category/serials/' in url: metaset='false'
+        match=re.compile('<a href="(.+?)" title="(.+?)">').findall(link)[:-12]
         for url,name in match:
                 name=cleanHex(name)
                 if metaset=='false':
-                        addDir(name,url,100,icon,len(match),isFolder=False)
+                        addLink(name,url,100,icon,'',fanart)
                 else: addDir(name,url,100,'',len(match),isFolder=False)
         try:
-                match=re.compile('"nextLink":"(.+?)"').findall(link)
-                url= match[0]
-                url = url.replace('\/','/')
-                addDir('Next Page>>',url,1,icon,len(match),isFolder=True)
+                url=re.compile("<link rel='next' href='(.+?)'/>").findall(link)[0]
+                addDir2('Next Page>>',url,1,icon,'',fanart)
         except: pass
         if metaset=='true':
                 setView('movies', 'MAIN')
@@ -55,55 +55,19 @@ def SEARCH():
     if keyboard.isConfirmed():
         search_entered = keyboard.getText().replace(' ','+')
     if len(search_entered)>1:
-        url = 'http://watch-onlinemovies.pro/?s='+ search_entered
+        url = 'http://onlinemovies.pro/?s='+ search_entered
         link = open_url(url)
         GETMOVIES(url,name)
 
 def PLAYLINK(name,url,iconimage):
         link = open_url(url)
-        try:
-            match=re.compile('php\?ref=(.+?)\&width').findall(link)
-            if len(match) > 0:
-                videomega_url = 'http://videomega.tv/?ref='+match[0]
-            else:
-                match=re.compile('src="http://videomega.tv/validatehash.php\?hashkey=(.+?)">').findall(link)
-                videomega_id_url = "http://videomega.tv/validatehash.php?hashkey="+ match[0]
-                req = urllib2.Request(videomega_id_url,None)
-                req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:34.0) Gecko/20100101 Firefox/34.0')
-                req.add_header('Referer', url)
-                response = urllib2.urlopen(req)
-                link=response.read()
-                response.close()
-                match=re.compile('var ref="(.+?)";').findall(link)[0]
-                videomega_url = 'http://videomega.tv/?ref='+match
-        except:
-                match=re.compile('src="http://videomega.tv/cdn.php?ref=(.+?)&').findall(link)
-                videomega_url = 'http://videomega.tv/?ref='+match[0]
-##RESOLVE##     
-        url = urlparse.urlparse(videomega_url).query
-        url = urlparse.parse_qs(url)['ref'][0]
-        url = 'http://videomega.tv/cdn.php?ref=%s' % url
-        referer = videomega_url
-        req = urllib2.Request(url,None)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        req.add_header('Referer', referer)
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()        
-        stream_url = re.compile('<source src="(.+?)" type="video/mp4"/>').findall(link)[0]
-        stream_url = stream_url + '|User-Agent: Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9100 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
-        
-##RESOLVE##
-        
-        playlist = xbmc.PlayList(1)
-        playlist.clear()
-        listitem = xbmcgui.ListItem(name, iconImage=icon, thumbnailImage=icon)
-        listitem.setInfo("Video", {"Title":name})
-        listitem.setProperty('mimetype', 'video/x-msvideo')
-        listitem.setProperty('IsPlayable', 'true')
-        playlist.add(stream_url,listitem)
-        xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-        xbmcPlayer.play(playlist)
+        try: url=re.compile('src="(.+?)" allowFullScreen></iframe>').findall(link)[0]
+        except: url=re.compile("src='(.+?)' allowFullScreen></iframe>").findall(link)[0]
+        stream_url=urlresolver.resolve(url) 
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage=icon,thumbnailImage=icon); liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        xbmc.Player ().play(stream_url, liz, False)
 
 def get_params():
         param=[]
@@ -121,6 +85,16 @@ def get_params():
                         if (len(splitparams))==2:
                                 param[splitparams[0]]=splitparams[1]
         return param
+
+def addLink(name,url,mode,iconimage,description,fanart):
+        xbmc.executebuiltin('Container.SetViewMode(50)')
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
 
 def addDir2(name,url,mode,iconimage,description,fanart):
         xbmc.executebuiltin('Container.SetViewMode(50)')
@@ -142,7 +116,8 @@ def addDir(name,url,mode,iconimage,itemcount,isFolder=False):
                 simpleyear=splitName[2].partition(')')
             if len(simpleyear)>0:
                 simpleyear=simpleyear[0]
-            meta = metaget.get_meta('movie', simplename ,simpleyear)
+            mg = metahandlers.MetaData()
+            meta = mg.get_meta('movie', name=simplename ,year=simpleyear)
             u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&site="+str(site)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
             ok=True
             liz=xbmcgui.ListItem(name, iconImage=meta['cover_url'], thumbnailImage=iconimage)
@@ -195,7 +170,6 @@ print params
 if mode==None or url==None or len(url)<1: CATEGORIES()
 elif mode==1: GETMOVIES(url,name)
 elif mode==2: GETTV(url,name)
-
 elif mode==3: SEARCH()
 elif mode==100: PLAYLINK(name,url,iconimage)
 
